651116729dc40be82cc7531912c4f0dc0efe7536
Zhiyao Wang <90308301+uqzhiyao@users.noreply.github.com>
2024-09-18 11:45:11 +1000

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,47 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Edinburgh, UK",
+    55.9533456,
+    -3.1883749
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Brisbane, Australia",
+    -27.4689682,
+    153.0234991
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Hong Kong",
+    22.350627,
+    114.1849161
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Hangzhou, China",
+    30.2489634,
+    120.2052342
+  ],
+  [
+    "Helsinki, Finland",
+    60.1674881,
+    24.9427473
+  ],
+  [
+    "Adelaide, Australia",
+    -34.9281805,
+    138.5999312
+  ],
+  [
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Hefei, China",
+    31.8665676,
+    117.281428
+  ],
+  [
+    "Tubingen, Germany",
+    48.5203263,
+    9.053596
   ]
 ];
